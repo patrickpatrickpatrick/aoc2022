af4b0b2d3dadc847e002a85ab26175d73637ab84
Patrick Cartlidge <patrickcartlidge@gmail.com>
2022-12-02 11:51:30 +0000

    day2pt2

--- day2.js
+++ day2.js
@@ -2,26 +2,47 @@ var lineReader = require('readline').createInterface({
   input: require('fs').createReadStream('day2_input.txt')
 });
 
-let curr = 0
+let curr1 = 0
+let curr2 = 0
 
 const scores = {
 	"B X": 0,
-	"C Y": 0,
-	"A Z": 0,
-	"A X": 3,
-	"B Y": 3,
-	"C Z": 3,
-	"C X": 6,
+	"C Y": 0, // - or could just make these the actual scores
+	"A Z": 0, //   don't really need to mod 3 of the index but
+	"A X": 3, //   that makes it more interesting lol
+	"B Y": 3, // - or could not use an object at all and just use
+	"C Z": 3, //   an array since could just use the indexes of
+	"C X": 6, //   the array (again wanted to be more interesting)
 	"A Y": 6,
 	"B Z": 6,
 };
 
+const scores2 = {
+	"B X": 0,
+	"C X": 0,	// - or could just make these the actual scores
+	"A X": 0,	//   don't really need to mod 3 of the index but
+	"A Y": 3,	//   that makes it more interesting lol
+	"B Y": 3,	// - or could not use an object at all and just use
+	"C Y": 3,	//   an array since could just use the indexes of
+	"C Z": 6,	//   the array (again wanted to be more interesting)
+	"A Z": 6,
+	"B Z": 6,
+}
+
 lineReader.on('line', function (line) {
-	curr += scores[line];
-	curr += (((Object.keys(scores).findIndex(x => x == line)) % 3) + 1)
+	// fine here's a util function to tidy it slightly
+	const getScore = (line, list) => ((Object.keys(list).findIndex(x => x == line)) % 3) + 1
+
+	curr1 += scores[line];
+	curr1 += getScore(line, scores);
+
+	curr2 += scores2[line];
+	curr2 += getScore(line, scores2);
 });
 
 lineReader.on('close', function (line) {
 	// part 1
-	console.log(curr);
+	console.log(curr1);
+	// part 2
+	console.log(curr2);
 });
