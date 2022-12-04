1db2667352298e4a5c4d258e48529ecb79f400c5
Patrick Cartlidge <patrickcartlidge@gmail.com>
2022-12-04 12:18:26 +0000

    day4js

--- day4.js
+++ day4.js
@@ -2,21 +2,29 @@ var lineReader = require('readline').createInterface({
   input: require('fs').createReadStream('day4_input.txt')
 });
 
-curr = 0;
+curr1 = 0;
+curr2 = 0;
 
 lineReader.on('line', function (line) {
   const input = line.match(/(\d+)\-(\d+)\,(\d+)\-(\d+)/);
-  const reduceFunction = list => (y, x) => ((x >= list[0]) && (x <= list[1])) && y;
+  const reduceFunction = (list, bool) => eval(
+    `(y, x) => ((x >= list[0]) && (x <= list[1])) ${bool} y`);
 
   const firstPair = (input.slice(1, 3)).map(x => parseInt(x));
   const secondPair = (input.slice(3, 5)).map(x => parseInt(x));
 
-  curr = curr + (
-          firstPair.reduce(reduceFunction(secondPair), true) ||
-          secondPair.reduce(reduceFunction(firstPair), true) * 1
+  curr1 = curr1 + (
+          firstPair.reduce(reduceFunction(secondPair, "&&"), true) ||
+          secondPair.reduce(reduceFunction(firstPair, "&&"), true) * 1
+        );
+
+  curr2 = curr2 + (
+          firstPair.reduce(reduceFunction(secondPair, "||"), false) ||
+          secondPair.reduce(reduceFunction(firstPair, "||"), false) * 1
         );
 });
 
 lineReader.on('close', () => {
-  console.log(curr)
+  console.log(curr1)
+  console.log(curr2)
 });
